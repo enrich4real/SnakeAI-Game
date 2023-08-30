2b9ad875e036f4e654aa622a317e9bd77ad43d20
enrich4real <prashant2003gupta@gmail.com>
2023-08-31 00:31:59 +0530

    Snake Game created

---
+++ agent.py
@@ -0,0 +1,59 @@
+import torch
+import random
+import numpy as np
+from snakeai import SnakeGameAI, Direction, Point
+from collections import deque
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR=0.001
+
+class Agent:
+
+    def __init__(self) :
+        self.n_games = 0
+        self.epsilon = 0 # randomness
+        self.gamma = 0 # discount factor
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        # TODO: model, trainer
+        
+
+    def get_state(self, game):
+        pass
+
+    def remember(self,state, action, reward, next_state, done):
+        pass
+
+
+    def train_long_memory(self):
+        pass
+
+    def train_short_memory(self):
+        pass
+
+    def get_action(self, state):
+        pass
+
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        # get old state
+        state_old =  agent.get_state(game)
+
+        #get move
+        final_move = agent.get_action(state_old)
+
+        # perform move and get new state
+        reward, done, score= game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        #train short memory
+        
+
+if __name__ == "__main__":
+    train()
